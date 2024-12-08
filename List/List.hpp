de087db2609525797cee2cbebbe6006f3bd5475f
Zubair Ahmad <zubairahmadcontact@gmail.com>
2024-12-08 15:07:16 +0500

    Create a Generic List Datastructure (#2)

---
+++ List/List.hpp
@@ -0,0 +1,231 @@
+#pragma once
+
+#include <stdexcept>
+
+template <typename T> class List {
+private:
+  T *list;     // It is the array which will store all the list elements
+  int current; // It is the current marker
+  int size;    // Size of the list
+
+public:
+  List();
+
+  // A copy constructor to ensure deep copy
+  List(const List<T> &);
+
+  // Returns the size of the list
+  int length() const;
+
+  // Returns the value at current index
+  T get() const;
+
+  // Adds the given element to the current position in list.
+  void add(const T &);
+
+  // Removes the current element from the list
+  void remove();
+
+  // Updates the current element with provided value
+  void update(const T &);
+
+  // Moves the current pointer to the start of the list, throws exception if
+  // list don't have any elements
+  void start();
+
+  // Moves the current pointer to the end of the list, throws exception if list
+  // don't have any elements
+  void end();
+
+  // Moves the current pointer/marker 1 step forward. If it can't move next
+  // (i.e, at the end of list) then false is returned otherwise true. If list
+  // don't have any elements, then exception is thrown
+  bool next();
+
+  // Moves the current pointer/marker 1 step backward. If it can't move backward
+  // (i.e, at the start of list) then false is returned otherwise true. If list
+  // don't have any elements, then exception is thrown
+  bool back();
+
+  // It moves the current marker to index at which, the given element is present
+  // It returns true if element is present, otherwise false
+  bool find(const T &);
+
+  // Destrutor to deallocate the allocated memory
+  ~List();
+};
+
+template <typename T> List<T>::List() : list(nullptr), current(-1), size(0) {}
+
+template <typename T> List<T>::List(const List<T> &other) {
+
+  size = other.size;
+
+  list = new T[size];
+
+  for (int i = 0; i < other.size; i++) {
+    list[i] = other.list[i];
+  }
+
+  // Initalize current to the start of the list
+  current = 0;
+}
+
+template <typename T> int List<T>::length() const {
+  // Return the length/size of the list
+  return size;
+}
+
+template <typename T> T List<T>::get() const {
+  if (current == -1) {
+    throw std::runtime_error("The list don't have any elements!");
+  }
+  return list[current];
+}
+
+template <typename T> void List<T>::add(const T &elem) {
+  if (list == nullptr) {
+    // If list is has no element, then simply create a new one.
+    list = new T[1];
+    list[0] = elem;
+    current = 0;
+  } else {
+    // Store current list in temporary array
+    T *temp = list;
+
+    // Reallocate list with increased size
+    list = new T[size + 1];
+
+    int oldListIndex = 0;
+
+    // Simply copy as long as oldListIndex < current
+    // If it is equal then insert the element and
+    // after that keep copying oldList at oldListIndex+1 in the new list.
+    while (oldListIndex < size) {
+      if (oldListIndex < current) {
+        list[oldListIndex] = temp[oldListIndex];
+      }
+      if (oldListIndex == current) {
+        list[oldListIndex] = elem;
+      }
+      if (oldListIndex >= current) {
+        list[oldListIndex+1] = temp[oldListIndex];
+      }
+      ++oldListIndex;
+    }
+
+    // Free the memory
+    delete[] temp;
+  }
+
+  // Increment the size
+  size++;
+}
+
+template <typename T> void List<T>::remove() {
+
+  if (list == nullptr)
+    throw std::runtime_error("The list is empty! Cannot remove element!");
+
+  if (size == 1) {
+    delete[] list;
+    list = nullptr;
+    current = -1;
+  } else {
+    // Store current list in temp
+    T *temp = list;
+
+    // Reallocate list with decreased size
+    list = new T[size - 1];
+
+    int oldListIndex = 0;
+    
+    // Simply copy as long as oldListIndex < current
+    // When oldListIndex == current then skip copying this time.
+    // Afterwards, copy oldList at oldListIndex-1 in new list
+    while (oldListIndex < size) {
+      if (oldListIndex < current) {
+        list[oldListIndex] = temp[oldListIndex];
+      }
+      if (oldListIndex > current) {
+        list[oldListIndex-1] = temp[oldListIndex];
+      }
+      ++oldListIndex;
+    }
+
+    // Free temporary memory
+    delete[] temp;
+
+    // If the current element was pointing at end, it should still point at end.
+    if (current == size-1)
+      --current;
+  }
+
+  // Decrement size after removal
+  --size;
+}
+
+template <typename T> void List<T>::update(const T &newValue) {
+  if (current == -1)
+    throw std::runtime_error("The list don't have any elements! Can't update current element!");
+  list[current] = newValue;
+}
+
+template <typename T> void List<T>::start() {
+  if (current == -1)
+    throw std::runtime_error("The list don't have any elements! Can't move to start!");
+  current = 0;
+}
+
+template <typename T> void List<T>::end() {
+  if (current == -1)
+    throw std::runtime_error("The list don't have any elements! Can't move to end!");
+  current = size - 1;
+}
+
+template <typename T> bool List<T>::next() {
+
+  if (current == -1)
+    throw std::runtime_error("The list don't have any element! Can't move forward!");
+
+  if (current == size - 1) {
+    return false;
+  } else {
+    ++current;
+    return true;
+  }
+}
+
+template <typename T> bool List<T>::back() {
+
+  if (current == -1)
+    throw std::runtime_error("The list don't have any element! Can't move backward!");
+
+  if (current == 0) {
+    return false;
+  } else {
+    --current;
+    return true;
+  }
+}
+
+template <typename T> bool List<T>::find(const T &elem) {
+
+  for (int i = 0; i < size; i++) {
+    if (list[i] == elem) {
+      // If the element is present, then simply move the current pointer/marker
+      // to this index.
+      current = i;
+      return true;
+    }
+  }
+  // The given element is not present in the list
+  return false;
+}
+
+template <typename T> List<T>::~List() {
+  if (list != nullptr) {
+    // Free the memory allocated for list
+    delete[] list;
+  }
+}
---
+++ List/list_test.cpp
@@ -0,0 +1,94 @@
+#include "List.hpp"
+#include <iostream>
+
+using namespace std;
+
+template <typename T> std::ostream &operator<<(std::ostream &op, List<T> list) {
+  if (list.length() == 0)
+    op << "[]";
+  else {
+    op << "[" << list.get();
+
+    while (list.next()) {
+      op << ", " << list.get();
+    }
+
+    op << "]";
+  }
+  return op;
+}
+
+void inputList(List<int> &list) {
+  int value = 0;
+
+  do {
+    cout << "\nEnter value to add in list (-1 to exit) : ";
+    cin >> value;
+
+    if (value != -1) {
+      list.add(value);
+    }
+  } while (value != -1);
+}
+
+template <typename T> void printInReverse(List<int> myList) {
+
+  cout << "\nReversed List : ";
+
+  // If list is empty then simply output this and return
+  if (myList.length() == 0) {
+    cout << "[]\n";
+    return;
+  }
+
+  // Move to end
+  myList.end();
+
+  cout << "[" << myList.get();
+  while (myList.back()) {
+    cout << ", " << myList.get();
+  }
+  cout << "]\n";
+}
+
+int main() {
+
+  List<int> myList;
+
+  inputList(myList);
+
+  cout << "\nmyList = " << myList;
+  printInReverse<int>(myList);
+
+  // Remove as many can be removed
+  try {
+    myList.end();
+
+    myList.remove();
+    myList.remove();
+    myList.remove();
+  } catch (const exception &e) { /* Just silent the exception */ }
+
+  cout << "\nMy list after some deletions from end: " << myList;
+  printInReverse<int>(myList);
+
+  int val;
+  cout << "\nEnter any element to check whether its present: ";
+  cin >> val;
+
+  if (myList.find(val)) {
+    cout << val << " is present in the list!\n";
+
+    cout << "\nWith what value, you want to change it : ";
+    cin >> val;
+
+    myList.update(val);
+
+    cout << "\nMy List: " << myList << endl;
+
+  } else {
+    cout << val << " is not present in the list!\n";
+  }
+
+  return 0;
+}
