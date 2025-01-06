a5c4fd7457841d03c0ab9870871bf9bb46dd7314
Zubair Ahmad <zubairahmadcontact@gmail.com>
2025-01-06 18:05:38 +0500

    Provide implementation for the interface

--- Stack/Array Stack/Stack.hpp
+++ Stack/Array Stack/Stack.hpp
@@ -19,3 +19,36 @@ class Stack {
     bool isEmpty() const;
     bool isFull() const;
 };
+
+inline Stack::Stack(): current(-1) {
+  // Initialize every element with 0
+  for (int i = 0; i < MAX_STACK_SIZE; i++) {
+    stackArray[i] = 0;
+  }
+}
+
+inline Stack::Stack(const Stack& other): current(other.current) {
+  for (int i = 0; i < MAX_STACK_SIZE; i++) {
+    stackArray[i] = other.stackArray[i];
+  }
+}
+
+inline void Stack::push(int elem) {
+  stackArray[++current] = elem;
+}
+
+inline int Stack::pop() {
+  return stackArray[current--];
+}
+
+inline int Stack::top() const {
+  return stackArray[current];
+}
+
+inline bool Stack::isEmpty() const {
+  return current == -1;
+}
+
+inline bool Stack::isFull() const {
+  return current == MAX_STACK_SIZE;
+}
